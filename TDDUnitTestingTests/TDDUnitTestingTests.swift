efad6ff8728a38188b566441dd9b96d61fe2d4b8
Ivan <izzyumkin@gmail.com>
2021-04-13 10:21:57 +0500

    Initialize test project

--- TDDUnitTestingTests/TDDUnitTestingTests.swift
+++ TDDUnitTestingTests/TDDUnitTestingTests.swift
@@ -24,47 +24,5 @@ class TDDUnitTestingTests: XCTestCase {
         super.tearDown()
     }
     
-    func testLowestVolumeShouldBeZero() {
-        
-        sut.setVolume(value: -100)
-        
-        let volume = sut.volume
-        
-        XCTAssert(volume == 0, "Lowest volume should be equal 0.")
-    }
-    
-    func testHighestVolumeShouldBe100() {
-        
-        sut.setVolume(value: 10000)
-        
-        let volume = sut.volume
-        
-        XCTAssert(volume == 100, "Highest volume should be equal 100.")
-        
-    }
-    
-    func testCharInStringsAreTheSame() {
-        
-        //given - дано
-        let firstString = "qwerty"
-        let secondString = "ytrewq"
-        //when - что произошло
-        let boolResult = sut.charactersCompare(firstString: firstString, secondString: secondString)
-        //then - какой результат должен получиться
-        XCTAssert(boolResult, "Characters should be the same in two strings")
-        
-    }
-    
-    func testCharInStringsAreDifferent() {
-        
-        //given - дано
-        let firstString = "qwerty"
-        let secondString = "ytrewq1"
-        //when - что произошло
-        let boolResult = sut.charactersCompare(firstString: firstString, secondString: secondString)
-        //then - какой результат должен получиться
-        XCTAssert(!boolResult, "Characters should be different in two strings")
-        
-    }
 
 }
